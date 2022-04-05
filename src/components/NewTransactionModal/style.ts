1096e62c1ef8dd2684609c3b79fb78df5f7eb42e
João Augusto Zanardo de Lima <augustozanardo@yahoo.com.br>
2022-04-05 14:03:18 -0300

    New margin bottom from transaction modal

--- src/components/NewTransactionModal/style.ts
+++ src/components/NewTransactionModal/style.ts
@@ -5,7 +5,8 @@ export const Container = styled.form`
     h2 {
         color: var(--text-title);
         font-size: 1.5rem;
-        margin-bottom: 2rem;
+        margin-bottom: 2.5rem;
+        
     }
 
     input {
